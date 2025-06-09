e84a0f01360679051816df983c6fa8924a0b315c
sohamm75 <sohamtodankar.business@gmail.com>
2025-06-09 14:10:18 +0530

    Delete user-management directory

--- user-management/src/main/java/com/example/usermanagement/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.usermanagement.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
--- user-management/src/main/java/com/example/usermanagement/repository/package-info.java
+++
@@ -1,2 +0,0 @@
-
-package com.example.usermanagement.repository;
